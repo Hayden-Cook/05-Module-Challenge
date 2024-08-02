8fd340043558062a88b594daadc8ea5c9bacd51c
HCook <166083664+Hayden-Cook@users.noreply.github.com>
2024-08-01 23:17:40 -0500

    added javascript and readme content

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,22 +4,85 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    const taskId = nextId++;
+    localStorage.setItem('nextId', JSON.stringify(nextId));
+    return taskId;
 }
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
+    const taskCard = $('<div>').addClass("card draggable");
+    const taskCardBody = $('<div>').addClass("taskcard-body");
+    const taskCardTitle = $('<h4').addClass("taskcard-title").text(task.title);
+    const taskCardContent = $('<p>').addClass("taskcard-content").text(task.content);
+    const taskCardDueDate = $('<p>').addClass("taskcard-duedate").text("Task Due: " + task.dueDate);
+    const deleteBtn = $("<button>").addClass("btn btn-danger btn-small").text("Delete");
+    deleteBtn.click(handleDeleteTask);
 
+    if (task.status && task.dueDate !== "done") {
+        let currentDay = dayjs();
+        let taskDueDate = dayjs(task.dueDate, "MM-DD-YYYY");
+
+        if (taskDueDate.isBefore(currentDay, "day")) {
+            taskCard.addClass("bg-danger text-white");
+        } else if (taskDueDate.isSame(currentDay, "day")) {
+            taskCard.addClass("bg-warning text-white");
+        } else if (taskDueDate.isAfter(currentDay, "day")) {
+            taskCard.addClass("bg-light text-dark");
+        }
+    }
+    taskCardBody.append(taskCardTitle, taskCardContent, taskCardDueDate, deleteBtn);
+    taskCard.append(taskCardBody);
+    return taskCard;
 }
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
+    // This should clear any cards on the page before rendering the new list.
+    $("#done-cards").empty();
+    $("#in-progress-cards").empty();
+    $("#done-cards").empty();
 
+    taskList.forEach(task => {
+        const card = createTaskCard(task);
+        //this will run through each task and apply the status before "-cards" and will then append it to the correct container.
+        $(`#${task.status}-cards`).append(card);
+    });
+    // this will make the cards draggable
+    $(".draggable").draggable({
+        zIndex: 100,
+    })
 }
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    event.preventDefault();
 
+    const taskTitle = $("#taskTitle").val().trim();
+    const taskDueDate = $("#taskDueDate").val().trim();
+    const taskDescription = $("#taskDescription").val().trim();
+
+    if (!taskDescription || !taskDueDate || !taskTitle) {
+        alert("Please complete all fields to continue.");
+        return;
+    }
+    const newTaskCard = {
+        id: generateTaskId(),
+        taskTitle,
+        taskDueDate,
+        taskDescription,
+        status: "to-do"
+    };
+
+    taskList.push(newTaskCard);
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+    // this will clear the contents and hide the modal box
+    $("#taskTitle").val("");
+    $("#taskDueDate").val("");
+    $("#taskDescription").val("");
+    $("#formModal").modal("hide");
+
+    renderTaskList();
 }
 
 // Todo: create a function to handle deleting a task
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Task Board Starter Code
+
+## Description
+This week, we had the chance to build a task board that would separate your cards based upon the current progress and due date. This assignment involved using dayjs and APIs. A user can utilize this tool to help keep track of what they need to complete and how soon they should complete it. 
+
+## Screenshot
+
+## Credits
+Module 5 Criteria
+https://bootcampspot.instructure.com/courses/5982/assignments/83901?module_item_id=1293261
+
+Module 5 Lesson Activities
+https://git.bootcampcontent.com/University-of-Texas-at-Austin/UTA-VIRT-FSF-PT-06-2024-U-LOLC
+
+Day.js
+https://day.js.org/docs/en/installation/installation
+
+## Deployment URL
+
+## License
+[MIT]
